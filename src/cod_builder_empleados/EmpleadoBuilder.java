7c1b5852bb579dd690d202d5348cf1bfe984f122
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2019-02-07 09:21:48 +0100

    2.ClassBuilder, asignamos valores por defecto

---
+++ src/cod_builder_empleados/EmpleadoBuilder.java
@@ -0,0 +1,31 @@
+/*
+ * BUILDER :
+Se va a encargar de los valores por defecto de los atributos, por lo que
+se asignan valores por defecto a los mismos
+ */
+package cod_builder_empleados;
+
+/**
+ *
+ * @author lvazquezdorna
+ */
+public class EmpleadoBuilder {
+
+    //atributos
+    private String nombre;
+    private String apellido1;
+    private String apellido2 = "";
+    private String dni;
+    private String cargo = "";
+    private int dia = 0;
+    private int mes = 0;
+    private int ano = 0;
+    private int altura = 0;
+    private int pie = 0;
+    private int diametro = 0;
+    private int sueldo = 0;
+    private int formacion = 0;
+
+    
+
+}
